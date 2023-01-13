33decbc565cf44e048d95f799f2b34abb46814d0
jagerziel <fromnytosg@gmail.com>
2023-01-13 05:24:13 -0500

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@
 import './App.css';
 //Import Components
 import Navbar from './Pages/HomeNavbar/Navbar.jsx';
+import { BrowserRouter , Routes , Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className='MainContent'>
+        <Routes>
+          {/* <Route path="/" element={<h2>Home!</h2>}/> */}
+        </Routes> 
+      </div>
     </div>
   );
 }
--- src/Pages/HomeNavbar/DataDL.jsx
+++ src/Pages/HomeNavbar/DataDL.jsx
@@ -1,8 +1,19 @@
 import './DataDL.css'
+import { useState , useEffect } from 'react';
 
 //Build Navbar Component
 function DataDL() {
+    const [ data , setData ] = useState({})
     //Assign Variables
+    useEffect(() => {
+        fetch(`https://projecttwoapi-production.up.railway.app/api/fish`)
+        .then(res => res.json())
+        .then(data => setData(data))
+    }, []);      
+    console.log(data)
+    
+
+
 
     return (   
         <>
