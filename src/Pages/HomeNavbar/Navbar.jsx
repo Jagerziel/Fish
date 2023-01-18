5b9f1f2e17552033ff45c8c2fd2f826be04fccc6
jagerziel <fromnytosg@gmail.com>
2023-01-18 07:28:49 -0500

    Cleaned up code in NavBar

--- src/Pages/HomeNavbar/Navbar.jsx
+++ src/Pages/HomeNavbar/Navbar.jsx
@@ -1,6 +1,5 @@
 //Imports
 import './Navbar.css'
-import DataDL from './DataDL.jsx'
 import { Link } from 'react-router-dom';
 
 //Build Navbar Component
@@ -8,7 +7,9 @@ function Navbar({setToggle}) {
     return (   
         <>
             <div className='NavbarContainer'>
-                <DataDL />
+                <div className='DataDL'>
+                    Exploring the Ocean
+                </div>
                 <div className='NavRight'>
                     <Link to={`/`} className="Links">Home</Link>
                     <Link to={`/random-fish`} onClick={()=> setToggle(prev => !prev)} className="Links" >Random Fish</Link>
